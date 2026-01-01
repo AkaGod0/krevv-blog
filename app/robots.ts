678653dcc40cfd85d0d65441fa0d3317c79a92cb
AkaGod0 <akavigo01@gmail.com>
2026-01-01 19:10:00 +0100

    Modify robots function to add disallow rules
    Updated robots function to include disallow rules for specific paths.

--- app/robots.ts
+++ app/robots.ts
@@ -1,17 +1,21 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL 
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://www.krevv.com";
 
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: [
-      `${baseUrl}/sitemap.xml`,
-      `${baseUrl}/sitemap-categories.xml`,
-      `${baseUrl}/rss.xml`,
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/web-man",
+          "/admin",
+          "/profile",
+          "/api/",
+        ],
+      },
     ],
+    sitemap: [`${baseUrl}/sitemap.xml`],
   };
 }
