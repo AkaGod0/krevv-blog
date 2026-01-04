62850e9f30066de5b2a378c1aec0f6b74f2fd844
AkaGod0 <akavigo01@gmail.com>
2026-01-04 22:23:33 +0100

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -18,20 +18,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     console.error("❌ Failed to fetch posts for sitemap:", err);
   }
 
-  
-  let jobs: any[] = [];
-  
-  try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/jobs?status=active&limit=0`, {
-      cache: "no-store"
-    });
-     
-      const data = await res.json();
-    jobs = Array.isArray(data.data) ? data.data : [];
-  } catch (err) {
-    console.error("❌ Failed to fetch jobs for sitemap:", err);
-  }
 
   // Static pages
   const staticRoutes: MetadataRoute.Sitemap = [
